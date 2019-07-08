b1bc19b93dc7038fe0fab438bf9ae38810bb7e70
Archit Agrawal <51698721+archit1998@users.noreply.github.com>
2019-07-08 18:48:30 +0530

    Add files via upload

---
+++ makefile.py
@@ -0,0 +1,35 @@
+import os
+
+command="pip install pandas"
+os.system(command)
+command="pip install xlrd"
+os.system(command)
+command="pip install numpy"
+os.system(command)
+command="pip install openpyxl"
+os.system(command)
+command="pip install DateTime"
+os.system(command)
+command="pip install xlwings"
+os.system(command)
+command="pip install fuzzywuzzy"
+os.system(command)
+command="pip install python-Levenshtein"
+os.system(command)
+command="pip install pyodbc"
+os.system(command)
+command="pip install configparser"
+os.system(command)
+command="pip install -U gensim "
+os.system(command)
+command="pip install nltk"
+os.system(command)
+command="pip install regex"
+os.system(command)
+command="pip install tabulate"
+os.system(command)
+
+import webbrowser
+
+webbrowser.open('https://www.microsoft.com/en-us/download/confirmation.aspx?id=56567')
+#https://www.microsoft.com/en-us/download/details.aspx?id=56567
